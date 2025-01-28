8c3d7b575a3d8aea6eda62ee161c3d0ad8d210dd
willmcroberts <williammcroberts15@gmail.com>
2025-01-28 17:08:16 +0000

    created and started horseRacing.cpp

---
+++ horseRacing.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+int main() {
+	int horses[5];
+	bool keepGoing = true;
+
+	while(keepGoing) {
+		for (int i = 0; i < 0; i++) {
+		
+		} // end for loop
+	} // end while loop
+
+	return 0;
+} // end main
