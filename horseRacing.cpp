c3c7640daf0c516b0ff75326dea14d299216b822
willmcroberts <williammcroberts15@gmail.com>
2025-01-28 19:11:47 +0000

    didn't actaully do anything

--- horseRacing.cpp
+++ horseRacing.cpp
@@ -5,13 +5,15 @@
 void advanceHorse(int, int*);
 
 int main() {
-	int horses[5];
+	int horses[5] = {0};
 	bool keepGoing = true;
 	const int TRACK_LENGTH = 15;
+	
+	srand(time(NULL));
 
 	while(keepGoing) {
 		for (int i = 0; i < TRACK_LENGTH; i++) {
-			advanceHorse(i, (&horses)[i]);
+			advanceHorse(i, horses);
 			for (int i = 0; i < 5; i++) {
 				std::cout << horses[i];
 			}
@@ -26,11 +28,10 @@ int main() {
 } // end main
 
 void advanceHorse(int horseNum, int (&horses)[]) {
-	srand(time(NULL));
 	for (int i = 0; i < 5; i++) {
 		int coin = rand() % 2;
 		if (coin == 1) {
-			horses[i]++;
+			horses[horseNum]++;
 		} // end elif statement
 	} // end for loop
 } // end advance
