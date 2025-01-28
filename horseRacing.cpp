76ae3a5fddcbe27e8b690df068517f339ee2ebff
willmcroberts <williammcroberts15@gmail.com>
2025-01-28 18:50:13 +0000

    added to horseRacing.cpp, but it has many errors

--- horseRacing.cpp
+++ horseRacing.cpp
@@ -2,15 +2,35 @@
 #include <cstdlib>
 #include <ctime>
 
+void advanceHorse(int, int*);
+
 int main() {
 	int horses[5];
 	bool keepGoing = true;
+	const int TRACK_LENGTH = 15;
 
 	while(keepGoing) {
-		for (int i = 0; i < 0; i++) {
-		
+		for (int i = 0; i < TRACK_LENGTH; i++) {
+			advanceHorse(i, (&horses)[i]);
+			for (int i = 0; i < 5; i++) {
+				std::cout << horses[i];
+			}
+		//	printLane(i, horses[i]);
+		//	if (isWinner(i, horses[i]) {
+		//		keepGoing = false;
+	//		} // end of if statement	
 		} // end for loop
 	} // end while loop
 
 	return 0;
 } // end main
+
+void advanceHorse(int horseNum, int (&horses)[]) {
+	srand(time(NULL));
+	for (int i = 0; i < 5; i++) {
+		int coin = rand() % 2;
+		if (coin == 1) {
+			horses[i]++;
+		} // end elif statement
+	} // end for loop
+} // end advance
