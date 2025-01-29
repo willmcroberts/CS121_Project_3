caa13d3e3ded5bfcb65433e7b2bc6105a468b133
willmcroberts <williammcroberts15@gmail.com>
2025-01-29 17:39:57 +0000

    Finished horseRacing.cpp

--- horseRacing.cpp
+++ horseRacing.cpp
@@ -21,24 +21,26 @@ int main() {
 				keepGoing = false;
 			} // end of if statement
 		} // end for loop
+		std::cout << "Press ENTER to continue";
+		std::cin.get();
 	} // end while loop
 
 	return 0;
 } // end main
 
 void advanceHorse(int horseNum, int* horses) {
-	for (int i = 0; i < 5; i++) {
-		int coin = rand() % 2;
-		if (coin == 1) {
-			horses[horseNum]++;
-		} // end elif statement
-	} // end for loop
+	int coin = rand() % 2;
+	if (coin == 1) {
+		horses[horseNum]++;
+	} // end elif statement
+
 } // end advanceHorse
 
 void printLane(int horseNum, int* horses) {
+	int postion = horses[horseNum];
 	for (int i = 0; i < TRACK_LENGTH; i++) {
-		if (i == horseNum) {
-			std::cout << horses[horseNum] << " ";
+		if (i == postion) {
+			std::cout << horseNum << " ";
 		} else {
 			std::cout << ". ";
 		} // end elif statement
@@ -51,7 +53,7 @@ bool isWinner(int horseNum, int* horses) {
 	bool result = false;
 	if (horses[horseNum] >= TRACK_LENGTH) {
 		result = true;
-		std::cout << "HORSE " << horses[horseNum] << " WINS!!!" << std::endl;
+		std::cout << "HORSE " << horseNum << " WINS!!!" << std::endl;
 	} // end if statement
 
 	return result;
